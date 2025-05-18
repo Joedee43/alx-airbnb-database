071d83eeabb71e36f1f147566c484b9a17903eea
joe dee <joedee4311@gmail.com>
2025-05-18 17:01:50 +0300

    add database-adv-script

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,9 +2,7 @@ SELECT bookings.id AS booking_id, users.id AS user_id, users.name, bookings.date
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
-SELECT properties.id AS property_id, properties.name, reviews.rating
-FROM properties
-LEFT JOIN reviews ON reviews.property_id = properties.id;
+SELECT p.*,r.* FROM Properties p LEFT JOIN Reviews r ON r.property_id = p.property_id ORDER BY p.property_id;
 
 SELECT users.id AS user_id, users.name, bookings.id AS booking_id, bookings.date
 FROM users
